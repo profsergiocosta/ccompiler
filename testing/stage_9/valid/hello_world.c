2ab2f76e397dfcb4b22398ef13655a2780df3705
sergio costa <sergio.costa@ufma.com>
2022-07-29 13:58:37 -0300

    update

--- testing/stage_9/valid/hello_world.c
+++ testing/stage_9/valid/hello_world.c
@@ -1,8 +1,7 @@
 
 int main() {
     int x = 72;
-    x = putchar(x);
-    /*
+    putchar(72);
     x = putchar(101);
     x = putchar(108);
     x = putchar(108);
@@ -15,7 +14,7 @@ int main() {
     x = putchar(108);
     x = putchar(100);
     x = putchar(33);
-   x =  putchar(10);
-   */
-  return 0;
+    x =  putchar(10);
+ 
+    return 0;
 }
